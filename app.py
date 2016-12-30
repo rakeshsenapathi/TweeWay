383a56cfe98b192139eecceb5d761a49e78db22f
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-30 19:23:14 +0530

    Integrated Tweepy to get follower list

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
 
+import tweepy
+import keys
+
 app = Flask(__name__)
+
+auth = tweepy.OAuthHandler(keys.getConsumerkey(),keys.getConsumerSecret())
+
+auth.set_access_token(keys.getAccessToken(),keys.getAccessSecret())
+
+api = tweepy.API(auth)
+
+followersList = api.followers_ids("718946692")
+
+for follower in followersList:
+	userdetails = api.get_user(follower)
+	print(userdetails.screen_name)
 
 #Decorator
 @app.route('/')
