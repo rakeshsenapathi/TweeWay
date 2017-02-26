7b770463f5fbfedd88ef55ec29a0e089587921bb
rakeshsenapathi <rakeshsenapathi@gmail.com>
2017-02-26 13:57:08 +0530

    Added temp file for tempory app.py changes

--- app.py
+++ app.py
@@ -7,47 +7,49 @@ import simplejson as json
 
 app = Flask(__name__)
 
-# auth = tweepy.OAuthHandler(keys.getConsumerkey(), keys.getConsumerSecret())
-
-# auth.set_access_token(keys.getAccessToken(), keys.getAccessSecret())
-
-# api = tweepy.API(auth)
-
-# followersList = []
-# follower = []
-
-# user_name = "user_name"
-
-# mCursor = tweepy.Cursor(api.followers, screen_name = user_name, count=200).items()
-
-# while True:
-#     try:
-#         follower = mCursor.next()
-#         followersList.append(follower.screen_name)
-#     except tweepy.TweepError:
-#         print("Rate Limit! . Sleeping for 15 minutes...")
-#         time.sleep(60 * 15)
-#         follower = mCursor.next()
-#         followersList.append(follower.screen_name)
-#     except StopIteration:
-#         break
-
-# if(len(followersList)):
-# 	print("Printing %d follower ids"%len(followersList))
-# 	print(followersList)
-
-#Decorator
-# 	print(followersList)
-
 @app.route('/') 
 def index():
 	return render_template("index.html")
 
+mInputName = " "
 
 @app.route('/receiveName', methods=['POST'])
 def receiveName():
 	mReceivedName = request.get_data()
+	mInputName = mReceivedName
 	return json.dumps({'status':'OK','user':mReceivedName});
+
+
+auth = tweepy.OAuthHandler(keys.getConsumerkey(), keys.getConsumerSecret())
+
+auth.set_access_token(keys.getAccessToken(), keys.getAccessSecret())
+
+api = tweepy.API(auth)
+
+followersList = []
+follower = []
+
+user_name = mInputName
+
+mCursor = tweepy.Cursor(api.followers, screen_name = user_name, count=200).items()
+
+while True:
+    try:
+        follower = mCursor.next()
+        followersList.append(follower.screen_name)
+    except tweepy.TweepError:
+        print("Rate Limit! . Sleeping for 15 minutes...")
+        time.sleep(60 * 15)
+        follower = mCursor.next()
+        followersList.append(follower.screen_name)
+    except StopIteration:
+        break
+
+if(len(followersList)):
+	print("Printing %d follower ids"%len(followersList))
+	print(followersList)
+
+
 
 if(__name__ == "__main__"):
 	app.run(debug = True)
---
+++ temp.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+
+import tweepy
+import keys
+import time
+import simplejson as json
+
+app = Flask(__name__)
+
+followersList = []
+follower = []
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/receiveName', methods=['POST'])
+def receiveName():
+    mReceivedName = request.get_data()
+
+    auth = tweepy.OAuthHandler(keys.getConsumerkey(),keys.getConsumerSecret())
+
+    auth.set_access_token(keys.getAccessToken(),keys.getAccessSecret())
+    api = tweepy.API(auth)
+
+    mCursor = tweepy.Cursor(api.followers, screen_name = mReceivedName, count=200).items()
+    while True:
+        try:
+            follower = mCursor.next()
+            followersList.append(follower.screen_name)
+    
+        except tweepy.TweepError:
+            print("Rate Limit! . Sleeping for 15 minutes...")
+            time.sleep(60 * 15)
+            follower = mCursor.next()
+            followersList.append(follower.screen_name)
+        except StopIteration:
+            break
+
+    if(len(followersList)):
+        print("Printing %d follower ids"%len(followersList))
+        print(followersList)
+    
+    # return json.dumps({'status':'OK','user':mReceivedName});
+    return render_template("followers.html", followersList = followersList)
+    
+
+if(__name__ == "__main__"):
+    app.run(debug = True)
