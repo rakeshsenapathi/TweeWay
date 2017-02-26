a4055f9ec13b607e0fd43c10bac2648be58c665d
rakeshsenapathi <rakeshsenapathi@gmail.com>
2017-02-26 18:03:04 +0530

    Followers List is now displayed in a table

--- app.py
+++ app.py
@@ -7,49 +7,43 @@ import simplejson as json
 
 app = Flask(__name__)
 
-@app.route('/') 
-def index():
-	return render_template("index.html")
-
-mInputName = " "
-
-@app.route('/receiveName', methods=['POST'])
-def receiveName():
-	mReceivedName = request.get_data()
-	mInputName = mReceivedName
-	return json.dumps({'status':'OK','user':mReceivedName});
-
-
-auth = tweepy.OAuthHandler(keys.getConsumerkey(), keys.getConsumerSecret())
-
-auth.set_access_token(keys.getAccessToken(), keys.getAccessSecret())
-
-api = tweepy.API(auth)
-
 followersList = []
 follower = []
 
-user_name = mInputName
 
-mCursor = tweepy.Cursor(api.followers, screen_name = user_name, count=200).items()
-
-while True:
-    try:
-        follower = mCursor.next()
-        followersList.append(follower.screen_name)
-    except tweepy.TweepError:
-        print("Rate Limit! . Sleeping for 15 minutes...")
-        time.sleep(60 * 15)
-        follower = mCursor.next()
-        followersList.append(follower.screen_name)
-    except StopIteration:
-        break
-
-if(len(followersList)):
-	print("Printing %d follower ids"%len(followersList))
-	print(followersList)
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
+@app.route('/receiveName', methods=['POST'])
+def receiveName():
+    mReceivedName = request.get_data()
+
+    auth = tweepy.OAuthHandler(keys.getConsumerkey(),keys.getConsumerSecret())
+
+    auth.set_access_token(keys.getAccessToken(),keys.getAccessSecret())
+    api = tweepy.API(auth)
+
+    mCursor = tweepy.Cursor(api.followers, screen_name = mReceivedName, count=200).items()
+    while True:
+        try:
+            follower = mCursor.next()
+            followersList.append(follower.screen_name)
+    
+        except tweepy.TweepError:
+            print("Rate Limit! . Sleeping for 15 minutes...")
+            time.sleep(60 * 15)
+            follower = mCursor.next()
+            followersList.append(follower.screen_name)
+        except StopIteration:
+            break
+
+@app.route('/show')
+def show():
+    return render_template('followers.html', followersList = followersList)
+
+    
 
 if(__name__ == "__main__"):
-	app.run(debug = True)
+    app.run(debug = True)
