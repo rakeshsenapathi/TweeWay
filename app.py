55f99f06500e256050428799008fb34c879499a5
rakeshsenapathi <rakeshsenapathi@gmail.com>
2017-09-25 01:20:03 +0530

    Ignore

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def receiveName():
 
 @app.route('/show')
 def show():
-    mfollowersList = json.dumps(followersList)
-    return render_template('followers.html', followersList = mfollowersList )
+    followersList = json.dumps(followersList)
+    return render_template('followers.html', followersList = followersList )
 
     
 
